ec37022dbff0977118fc69e99ed9c508cc595842
Sajib Jahan <safallwa@gmail.com>
2024-12-04 18:31:17 +0600

    add test

---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingEndpoint(t *testing.T) {
+	router := SetupRouter()
+
+	req, err := http.NewRequest(http.MethodGet, "/ping", nil)
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, req)
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	expected := `{"message":"pong"}`
+	assert.JSONEq(t, expected, recorder.Body.String())
+}
